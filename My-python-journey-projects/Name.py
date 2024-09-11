bc4eaf558b508b3f08341df8df93e7fecb64a5c6
caleb ghanney <ghanneycaleb77@gmail.com>
2024-09-11 04:41:21 +0000

    add first project folder

---
+++ My-python-journey-projects/Name.py
@@ -0,0 +1,15 @@
+#  MY PYTHON BAND NAME GENERATOR
+
+# Displays the content of the double quote in the Console.
+print("Welcome to my Band Name Geberator")
+
+# Variable(city): stores the input received from the user
+city= input("What is the name of your City \n")
+
+# Variable(pet): stores the input received from the user
+pet= input("What is the name of your pet \n")
+
+# Displays the content within the Parentheses.
+print("Your Band name could be: " + city + " " +pet)
+
+# Note: \n is an escape modifier for a new line
